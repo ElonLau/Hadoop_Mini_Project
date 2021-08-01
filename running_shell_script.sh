d3d5c17db1c8ed92191a446d0a6250c15efeb197
ElonLau <52990733+ElonLau@users.noreply.github.com>
2021-08-01 15:26:03 +0800

    Add files via upload

---
+++ running_shell_script.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+echo "Running 1st set of mapper and reducer"
+
+PATH_TO_JAR=/path/to/hadoop_jar_file/hadoop-streaming-3.2.1.jar
+
+hadoop jar $PATH_TO_JAR \
+-file autoinc_mapper1.py -mapper autoinc_mapper1.py \
+-file autoinc_reducer1.py -reducer autoinc_reducer1.py \
+-input input/data.csv -output output/all_accidents
+
+echo "Running 2nd set of mapper and reducer"
+
+hadoop jar $PATH_TO_JAR \
+-file autoinc_mapper2.py -mapper autoinc_mapper2.py \
+-file autoinc_reducer2.py -reducer autoinc_reducer2.py \
+-input output/all_accidents -output output/make_year_count
